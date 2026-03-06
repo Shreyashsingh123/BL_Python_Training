9376d23071708c788bc415e5179ede6ebaa3da81
Shreyashsingh123 <singhshreyash631@gmail.com>
2026-03-06 17:50:56 +0530

    Added NumberPatternvalidator.py ,Solved Number Pattern using Loops

---
+++ NumberPatternvalidator.py
@@ -0,0 +1,22 @@
+num=int(input("Enter a number:  "))
+nums=[]
+i=0
+while(num>0):
+    r=num%10
+    num//=10
+    nums.append(r)
+ans=True
+nums.reverse()
+for i in range(len(nums)-1):
+    if({nums[i]<nums[i+1]}):
+        ans=True
+        continue
+    else:
+        ans=False
+        break
+if(ans):
+    print("YES")
+else:
+    print("NO")
+
+    
