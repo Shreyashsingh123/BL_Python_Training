f9cbce01a2382dde4f12438aed0dfbb0fb3bc283
Shreyashsingh123 <singhshreyash631@gmail.com>
2026-03-09 13:43:46 +0530

    Added Quadratic.py , calculated root of equation using formula

---
+++ Week-1/FunctionsProgram/Quadratic.py
@@ -0,0 +1,19 @@
+import math
+a = int(input("Enter value of a: "))
+b = int(input("Enter value of b: "))
+c = int(input("Enter value of c: "))
+
+d = b*b - 4*a*c
+# d is denoting to  delta 
+if d > 0:
+    r1 = (-b + math.sqrt(d)) / (2*a)
+    r2 = (-b - math.sqrt(d)) / (2*a)
+    print("Root 1:", r1)
+    print("Root 2:", r2)
+
+elif d == 0:
+    r = -b / (2*a)
+    print("Both roots are equal:", r)
+
+else:
+    print("Roots are imaginary (no real roots)")
